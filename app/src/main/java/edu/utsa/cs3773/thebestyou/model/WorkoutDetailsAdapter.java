9529412133860d925aff434519c775b2e96028b4
Marlet Garcia <marlet_garcia@yahoo.com>
2024-04-26 02:09:56 -0500

    successfully loads the details when the user clicks the challenge in SelectedChallengesActivity

---
+++ app/src/main/java/edu/utsa/cs3773/thebestyou/model/WorkoutDetailsAdapter.java
@@ -0,0 +1,54 @@
+package edu.utsa.cs3773.thebestyou.model;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import edu.utsa.cs3773.thebestyou.R;
+
+public class WorkoutDetailsAdapter extends RecyclerView.Adapter<WorkoutDetailsAdapter.ViewHolder> {
+
+    private List<ChallengeDetail.Exercise> exercises;
+
+    public WorkoutDetailsAdapter(List<ChallengeDetail.Exercise> exercises) {
+        this.exercises = exercises;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.exercise_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        ChallengeDetail.Exercise exercise = exercises.get(position);
+        holder.exerciseName.setText(exercise.getExerciseName());
+        holder.exerciseDuration.setText(String.valueOf(exercise.getExerciseDuration()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return exercises.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView exerciseName;
+        public TextView exerciseDuration;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            exerciseName = itemView.findViewById(R.id.exercise_name);
+            exerciseDuration = itemView.findViewById(R.id.exercise_duration);
+        }
+    }
+}
